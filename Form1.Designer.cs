00c1e0d6efff2e6cff6e3911245f7f712bd92b8d
Melisa Isik <melisaisik1068@hotmail.com>
2024-06-09 17:25:08 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace final_odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label20 = new Label();
+            label19 = new Label();
+            label18 = new Label();
+            label17 = new Label();
+            label16 = new Label();
+            label15 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            imageList1 = new ImageList(components);
+            timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetDouble;
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(label20, 4, 3);
+            tableLayoutPanel1.Controls.Add(label19, 3, 3);
+            tableLayoutPanel1.Controls.Add(label18, 2, 3);
+            tableLayoutPanel1.Controls.Add(label17, 1, 3);
+            tableLayoutPanel1.Controls.Add(label16, 0, 3);
+            tableLayoutPanel1.Controls.Add(label15, 4, 2);
+            tableLayoutPanel1.Controls.Add(label14, 3, 2);
+            tableLayoutPanel1.Controls.Add(label13, 2, 2);
+            tableLayoutPanel1.Controls.Add(label12, 1, 2);
+            tableLayoutPanel1.Controls.Add(label11, 0, 2);
+            tableLayoutPanel1.Controls.Add(label10, 4, 1);
+            tableLayoutPanel1.Controls.Add(label9, 3, 1);
+            tableLayoutPanel1.Controls.Add(label8, 2, 1);
+            tableLayoutPanel1.Controls.Add(label7, 1, 1);
+            tableLayoutPanel1.Controls.Add(label6, 0, 1);
+            tableLayoutPanel1.Controls.Add(label5, 4, 0);
+            tableLayoutPanel1.Controls.Add(label4, 3, 0);
+            tableLayoutPanel1.Controls.Add(label3, 2, 0);
+            tableLayoutPanel1.Controls.Add(label2, 1, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(778, 434);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label20
+            // 
+            label20.Location = new Point(626, 324);
+            label20.Name = "label20";
+            label20.Size = new Size(146, 99);
+            label20.TabIndex = 19;
+            label20.Click += Label_Click;
+            // 
+            // label19
+            // 
+            label19.Location = new Point(471, 324);
+            label19.Name = "label19";
+            label19.Size = new Size(142, 99);
+            label19.TabIndex = 18;
+            label19.Click += Label_Click;
+            // 
+            // label18
+            // 
+            label18.Location = new Point(316, 324);
+            label18.Name = "label18";
+            label18.Size = new Size(142, 99);
+            label18.TabIndex = 17;
+            label18.Click += Label_Click;
+            // 
+            // label17
+            // 
+            label17.Location = new Point(161, 324);
+            label17.Name = "label17";
+            label17.Size = new Size(143, 99);
+            label17.TabIndex = 16;
+            label17.Click += Label_Click;
+            // 
+            // label16
+            // 
+            label16.Location = new Point(6, 324);
+            label16.Name = "label16";
+            label16.Size = new Size(142, 99);
+            label16.TabIndex = 15;
+            label16.Click += Label_Click;
+            // 
+            // label15
+            // 
+            label15.Location = new Point(626, 217);
+            label15.Name = "label15";
+            label15.Size = new Size(146, 100);
+            label15.TabIndex = 14;
+            label15.Click += Label_Click;
+            // 
+            // label14
+            // 
+            label14.Location = new Point(471, 217);
+            label14.Name = "label14";
+            label14.Size = new Size(142, 99);
+            label14.TabIndex = 13;
+            label14.Click += Label_Click;
+            // 
+            // label13
+            // 
+            label13.Location = new Point(316, 217);
+            label13.Name = "label13";
+            label13.Size = new Size(142, 99);
+            label13.TabIndex = 12;
+            label13.Click += Label_Click;
+            // 
+            // label12
+            // 
+            label12.Location = new Point(161, 217);
+            label12.Name = "label12";
+            label12.Size = new Size(142, 99);
+            label12.TabIndex = 11;
+            label12.Click += Label_Click;
+            // 
+            // label11
+            // 
+            label11.Location = new Point(6, 217);
+            label11.Name = "label11";
+            label11.Size = new Size(142, 99);
+            label11.TabIndex = 10;
+            label11.Click += Label_Click;
+            // 
+            // label10
+            // 
+            label10.Location = new Point(626, 110);
+            label10.Name = "label10";
+            label10.Size = new Size(146, 99);
+            label10.TabIndex = 9;
+            label10.Click += Label_Click;
+            // 
+            // label9
+            // 
+            label9.Location = new Point(471, 110);
+            label9.Name = "label9";
+            label9.Size = new Size(142, 99);
+            label9.TabIndex = 8;
+            label9.Click += Label_Click;
+            // 
+            // label8
+            // 
+            label8.Location = new Point(316, 110);
+            label8.Name = "label8";
+            label8.Size = new Size(142, 99);
+            label8.TabIndex = 7;
+            label8.Click += Label_Click;
+            // 
+            // label7
+            // 
+            label7.Location = new Point(161, 110);
+            label7.Name = "label7";
+            label7.Size = new Size(142, 99);
+            label7.TabIndex = 6;
+            label7.Click += Label_Click;
+            // 
+            // label6
+            // 
+            label6.Location = new Point(6, 110);
+            label6.Name = "label6";
+            label6.Size = new Size(142, 99);
+            label6.TabIndex = 5;
+            label6.Click += Label_Click;
+            // 
+            // label5
+            // 
+            label5.Location = new Point(626, 3);
+            label5.Name = "label5";
+            label5.Size = new Size(146, 99);
+            label5.TabIndex = 4;
+            label5.Click += Label_Click;
+            // 
+            // label4
+            // 
+            label4.Location = new Point(471, 3);
+            label4.Name = "label4";
+            label4.Size = new Size(142, 100);
+            label4.TabIndex = 3;
+            label4.Click += Label_Click;
+            // 
+            // label3
+            // 
+            label3.Location = new Point(316, 3);
+            label3.Name = "label3";
+            label3.Size = new Size(143, 99);
+            label3.TabIndex = 2;
+            label3.Click += Label_Click;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(161, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 100);
+            label2.TabIndex = 1;
+            label2.Click += Label_Click;
+            // 
+            // label1
+            // 
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(6, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 99);
+            label1.TabIndex = 0;
+            label1.Click += Label_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "zany-face_1f92a.png");
+            imageList1.Images.SetKeyName(1, "face-with-monocle_1f9d0.png");
+            imageList1.Images.SetKeyName(2, "face-with-tears-of-joy_1f602.png");
+            imageList1.Images.SetKeyName(3, "ghost_1f47b.png");
+            imageList1.Images.SetKeyName(4, "heart-hands_1faf6.png");
+            imageList1.Images.SetKeyName(5, "melting-face_1fae0.png");
+            imageList1.Images.SetKeyName(6, "partying-face_1f973.png");
+            imageList1.Images.SetKeyName(7, "smiling-face-with-sunglasses_1f60e.png");
+            imageList1.Images.SetKeyName(8, "speak-no-evil-monkey_1f64a.png");
+            imageList1.Images.SetKeyName(9, "woozy-face_1f974.png");
+            // 
+            // timer1
+            // 
+            timer1.Interval = 300000;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(290, 437);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 48);
+            button1.TabIndex = 1;
+            button1.Text = "Yeni Oyun";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
+            ClientSize = new Size(774, 497);
+            Controls.Add(button1);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Resim Eşleştirme Oyunu";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label20;
+        private Label label19;
+        private Label label18;
+        private Label label17;
+        private Label label16;
+        private Label label15;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ImageList imageList1;
+        public System.Windows.Forms.Timer timer1;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics;
+
+namespace final_odev
+{
+    public partial class Form1 : Form
+    {
+        private Label firstClicked = null;
+        private Label secondClicked = null;
+        private System.Threading.Timer gameTimer;
+        private int matchedPairs = 0; // Eşleşen çiftlerin sayısını takip eden değişken
+        private Stopwatch stopwatch; // Oyun süresini takip eden nesne
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.button1_Click); // Yeni Oyun butonunun Click olayını ekleyin
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+        private void InitializeGame()
+        {
+            // TableLayoutPanel'e rastgele ikonları atayın
+            Random random = new Random();
+            List<int> icons = new List<int>()
+        {
+            0, 0, 1, 1, 2, 2, 3, 3, 4, 4,
+            5, 5, 6, 6, 7, 7, 8, 8, 9, 9
+        }; // 10 farklı ikon, her biri iki kez
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Label label)
+                {
+                    int randomIndex = random.Next(icons.Count);
+                    int iconIndex = icons[randomIndex];
+                    icons.RemoveAt(randomIndex);
+                    label.Tag = iconIndex;
+                    label.Click += new EventHandler(Label_Click);
+                }
+            }
+            // Eşleşen çiftlerin sayısını sıfırla
+            matchedPairs = 0;
+
+            // İlk ve ikinci tıklanan Label'ları sıfırla
+            firstClicked = null;
+            secondClicked = null;
+            // Stopwatch başlatın
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            if (firstClicked != null && secondClicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+            if (clickedLabel == null || clickedLabel.Image != null)
+                return;
+
+            int iconIndex = (int)clickedLabel.Tag;
+            clickedLabel.Image = imageList1.Images[iconIndex];
+
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                return;
+            }
+
+            secondClicked = clickedLabel;
+            CheckForMatch();
+        }
+        private void CheckForMatch()
+        {
+            if (firstClicked.Tag.Equals(secondClicked.Tag))
+            {
+                // Eşleşme varsa, etiketleri null yaparak sonraki tıklamalara hazır hale getirin
+                firstClicked = null;
+                secondClicked = null;
+                // Eşleşen çiftlerin sayısını artır
+                matchedPairs++;
+                // Oyunun tamamlanıp tamamlanmadığını kontrol edin
+                CheckForCompletion();
+                return;
+            }
+
+            // Eşleşme yoksa, resimleri kapatmak için bir Timer ayarlayın
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 2000; //2 saniye bekleyin
+            timer.Tick += (s, e) =>
+            {
+                firstClicked.Image = null;
+                secondClicked.Image = null;
+                firstClicked = null;
+                secondClicked = null;
+                timer.Stop();
+            };
+            timer.Start();
+        }
+        private void CheckForCompletion()
+        {
+            // 10 çift eşleştiğinde oyunu bitirin
+            if (matchedPairs == 10)
+            {
+                stopwatch.Stop(); // Stopwatch durdur
+                MessageBox.Show("Tebrikler!Bütün emojileri eşleştirdiniz:)");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            // Tüm eşleşen resimleri kapatın
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Label label)
+                {
+                    label.Image = null; // Eşleşen resimleri kapat
+                }
+            }
+            InitializeGame();
+        }
+    }
+}
